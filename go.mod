82bf3e83c57cfcc09e9c89d9ce593463bc5c75c0
ccppoo <shmishmi79@gmail.com>
2025-02-20 16:09:07 +0900

    envs

--- go.mod
+++ go.mod
@@ -12,7 +12,6 @@ require (
 	github.com/gin-gonic/gin v1.10.0
 	github.com/golang-jwt/jwt/v5 v5.2.1
 	github.com/google/uuid v1.6.0
-	github.com/joho/godotenv v1.5.1
 	gorm.io/gorm v1.25.12
 )
 
--- main.go
+++ main.go
@@ -13,7 +13,6 @@ import (
 	ginadapter "github.com/awslabs/aws-lambda-go-api-proxy/gin"
 	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
-	"github.com/joho/godotenv"
 
 	middleware "mc-dns-image-lambda/middleware"
 
@@ -21,47 +20,29 @@ import (
 	"github.com/aws/aws-sdk-go-v2/service/s3"
 )
 
-// This function loads the `.env` file.
-//
-// # The file should contain your AWS credentials
-//
-// AWS_REGION - AWS_ACCESS_KEY - AWS_ACCESS_KEY_ID
-func LoadDotEnv() {
-	if err := godotenv.Load(); err != nil {
-		log.Fatal("Error loading .env file:", err)
-	}
-}
-
 var (
 	ginLambda *ginadapter.GinLambda
 	s3Client  *s3.Client
 )
 
 func init() {
-	// LoadDotEnv()
 
-	temp := os.Getenv("BUCKET_PUBLIC")
-	log.Printf("BUCKET_PUBLIC %s", temp)
+	origin := os.Getenv("ALLOW_ORIGINS")
+
+	setUpAWS()
 
 	router := gin.Default()
 
 	router.Use(cors.New(cors.Config{
-		AllowOrigins:     []string{"https://localhost:5173", "https://mc-server.kr"}, // Replace with allowed origins
+		AllowOrigins:     []string{origin},
 		AllowMethods:     []string{"GET", "POST", "OPTIONS"},
 		AllowHeaders:     []string{"Content-Type", "Authorization", "token"},
 		ExposeHeaders:    []string{"Content-Length"},
 		AllowCredentials: true,
-		MaxAge:           24 * time.Hour, // Cache preflight response duration
+		MaxAge:           24 * time.Hour,
 	}))
 
 	router.Use(middleware.AuthMiddleware())
-
-	cfg, err := config.LoadDefaultConfig(context.TODO())
-	if err != nil {
-		log.Fatalf("unable to load SDK config, %v", err)
-	}
-	s3Client = s3.NewFromConfig(cfg)
-	handlers.SetS3Client(s3Client)
 
 	router.GET("/s3", handlers.ListObjects)
 
@@ -70,9 +51,18 @@ func init() {
 		uploader_route.POST("/image", handlers.UploadObject)
 		uploader_route.POST("/logo", handlers.UploadIcon)
 	}
-	// router.POST("/upload/image", handlers.UploadObject)
 
 	ginLambda = ginadapter.New(router)
+}
+
+func setUpAWS() {
+	cfg, err := config.LoadDefaultConfig(context.TODO(), config.WithRegion(os.Getenv("AWS_REGION")))
+
+	if err != nil {
+		log.Fatalf("unable to load SDK config, %v", err)
+	}
+	s3Client = s3.NewFromConfig(cfg)
+	handlers.SetS3Client(s3Client)
 }
 
 func GinRequestHandler(ctx context.Context, request events.APIGatewayProxyRequest) (events.APIGatewayProxyResponse, error) {
